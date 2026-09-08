662627c244aa615d0f2ecee41c48a5ba8495743c
NB-Media-Dev <trainee.developer@nbmediatech.org>
2026-09-08 10:10:46 +0530

    allow Vercel frontend origin in CORS

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://talktamila-adminpanel-s8pg.vercel.app"],
+        "https://talktamila-adminpanel-s8pg.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
